a0515a54c5692ff981b19a8e286dfdfe223c947f
Alex Olshansky <alex@creagenics.com>
2022-04-17 00:47:22 +0200

    Add Fun layer

--- qmk/keymap.c
+++ qmk/keymap.c
@@ -10,12 +10,14 @@
 #define L_NAV 3
 #define L_SYM 5
 #define L_NUM 6
+#define L_FUN 7
 
 // Top thumb rows: layer switching (turn on `layer` when held, `kc` when tapped)
 #define LT_MED  LT(L_MEDIA, KC_ESC)
 #define LT_NAV  LT(L_NAV,   KC_SPACE)
 #define LT_SYM  LT(L_SYM,   KC_ENTER)
 #define LT_NUM  LT(L_NUM,   KC_BACKSPACE)
+#define LT_FUN  LT(L_FUN,   KC_DEL)
 
 // Bottom thumb rows: layer switching (turn on `layer` and turn off other layers)
 #define TO_ENGR TO(L_ENGRAM)
@@ -74,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *  │       ║   G   ║   X   ║   J   ║   K   ║  - _  │                              │  ? !  ║   R   ║   M   ║   F   ║   P   ║  / ?  │
      *  └───────╨───────╬═══════╬═══════╬───────╨───────┘                              └───────╨───────╬═══════╬═══════╬───────╨───────┘
      *                  │       ║       │    ┌───────╥───────╥───────┐    ┌───────╥───────╥───────┐    │       ║       │
-     *                  └───────╨───────┘    │Esc Med║Spc Nav║  Tab  │    │Ent Sym║Bsp Num║  Del  │    └───────╨───────┘
+     *                  └───────╨───────┘    │Esc Med║Spc Nav║  Tab  │    │Ent Sym║Bsp Num║Del Fun│    └───────╨───────┘
      *                                       └───────╬═══════╬═══════╡    ╞═══════╬═══════╬───────┘
      *                                               │Engram ║ Latin │    │  Cyr  ║ Qwerty│
      *                                               └───────╨───────┘    └───────╨───────┘
@@ -83,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, KC_B,    KC_Y,    KC_O,    KC_U,    KC_9,                                  KC_0,    KC_L,    KC_D,    KC_W,    KC_V,    KC_Z,
     KC_GRV,  E_HRM_C, E_HRM_I, E_HRM_E, E_HRM_A, KC_COMM,                               KC_DOT,  E_HRM_H, E_HRM_T, E_HRM_S, E_HRM_N, KC_Q,
     XXXXXXX, KC_G,    KC_X,    KC_J,    KC_K,    KC_MINS,                               U_QUEX,  KC_R,    KC_M,    KC_F,    KC_P,    KC_SLASH,
-                      XXXXXXX, XXXXXXX,     LT_MED,  LT_NAV,  KC_TAB,      LT_SYM,  LT_NUM,  KC_DEL,      XXXXXXX, XXXXXXX,
+                      XXXXXXX, XXXXXXX,     LT_MED,  LT_NAV,  KC_TAB,      LT_SYM,  LT_NUM,  LT_FUN,      XXXXXXX, XXXXXXX,
                                                      TO_ENGR, HO_LNG1,     HO_LNG2, TO_QWER
   ),
 
@@ -140,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_NAV] = LAYOUT_5x6_5(
     /*
      *  ┌───────╥───────╥───────╥───────╥───────╥───────┐                              ┌───────╥───────╥───────╥───────╥───────╥───────┐
-     *  │ Reset ║       ║       ║       ║       ║       │                              │  F7   ║  F8   ║  F9   ║  F10  ║  F11  ║  F12  │
+     *  │ Reset ║       ║       ║       ║       ║       │                              │       ║       ║       ║       ║       ║       │
      *  ╞═══════╬═══════╬═══════╬═══════╬═══════╬═══════╡                              ╞═══════╬═══════╬═══════╬═══════╬═══════╬═══════╡
      *  │       ║       ║       ║       ║       ║       │                              │ Redo  ║ Paste ║ Copy  ║  Cut  ║ Undo  ║       │
      *  ╞═══════╬═══════╬═══════╬═══════╬═══════╬═══════╡                              ╞═══════╬═══════╬═══════╬═══════╬═══════╬═══════╡
@@ -154,7 +156,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                                               │       ║       │    │       ║       │
      *                                               └───────╨───────┘    └───────╨───────┘
      */
-    RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                               KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+    RESET,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                               U_REDO,  U_PASTE, U_COPY,  U_CUT,   U_UNDO,  XXXXXXX,
     XXXXXXX, KC_LCTL, KC_LOPT, KC_LCMD, KC_LSFT, XXXXXXX,                               KC_PGUP, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                               KC_PGDN, U_HOME,  U_WRD_L, U_WRD_R, U_END,   XXXXXXX,
@@ -190,7 +192,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_NUM] = LAYOUT_5x6_5(
     /*
      *  ┌───────╥───────╥───────╥───────╥───────╥───────┐                              ┌───────╥───────╥───────╥───────╥───────╥───────┐
-     *  │  F1   ║  F2   ║  F3   ║  F4   ║  F5   ║  F6   │                              │       ║       ║       ║       ║       ║       │
+     *  │       ║       ║       ║       ║       ║       │                              │       ║       ║       ║       ║       ║       │
      *  ╞═══════╬═══════╬═══════╬═══════╬═══════╬═══════╡                              ╞═══════╬═══════╬═══════╬═══════╬═══════╬═══════╡
      *  │       ║  [ {  ║  7 &  ║  8 *  ║  9 (  ║  ] }  │                              │       ║       ║       ║       ║       ║       │
      *  ╞═══════╬═══════╬═══════╬═══════╬═══════╬═══════╡                              ╞═══════╬═══════╬═══════╬═══════╬═══════╬═══════╡
@@ -204,11 +206,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                                               │       ║       │    │       ║       │
      *                                               └───────╨───────┘    └───────╨───────┘
      */
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     XXXXXXX, KC_SCLN, KC_4,    KC_5,    KC_6,    KC_EQL,                                XXXXXXX, KC_RSFT, KC_RCMD, KC_ROPT, KC_RCTL, XXXXXXX,
     XXXXXXX, KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                       KC_LT,   KC_GT,       KC_PDOT, KC_0,    KC_MINS,     XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX,
+                                                     XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX
+  ),
+
+  [L_FUN] = LAYOUT_5x6_5(
+    /*
+     *  ┌───────╥───────╥───────╥───────╥───────╥───────┐                              ┌───────╥───────╥───────╥───────╥───────╥───────┐
+     *  │       ║       ║       ║       ║       ║       │                              │       ║       ║       ║       ║       ║       │
+     *  ╞═══════╬═══════╬═══════╬═══════╬═══════╬═══════╡                              ╞═══════╬═══════╬═══════╬═══════╬═══════╬═══════╡
+     *  │       ║  F12  ║  F7   ║  F8   ║  F9   ║       │                              │       ║       ║       ║       ║       ║       │
+     *  ╞═══════╬═══════╬═══════╬═══════╬═══════╬═══════╡                              ╞═══════╬═══════╬═══════╬═══════╬═══════╬═══════╡
+     *  │       ║  F11  ║  F4   ║  F5   ║  F6   ║       │                              │       ║ Shift ║  Cmd  ║  Opt  ║ Ctrl  ║       │
+     *  ╞═══════╬═══════╬═══════╬═══════╬═══════╬═══════╡                              ╞═══════╬═══════╬═══════╬═══════╬═══════╬═══════╡
+     *  │       ║  F10  ║  F1   ║  F2   ║  F3   ║       │                              │       ║       ║       ║       ║       ║       │
+     *  └───────╨───────╬═══════╬═══════╬───────╨───────┘                              └───────╨───────╬═══════╬═══════╬───────╨───────┘
+     *                  │       ║       │    ┌───────╥───────╥───────┐    ┌───────╥───────╥┬┬┬┬┬┬┬┐    │       ║       │
+     *                  └───────╨───────┘    │  Esc  ║ Space ║  Tab  │    │       ║       ║│┃┃┃┃┃││    └───────╨───────┘
+     *                                       └───────╬═══════╬═══════╡    ╞═══════╬═══════╬┴┴┴┴┴┴┴┘
+     *                                               │       ║       │    │       ║       │
+     *                                               └───────╨───────┘    └───────╨───────┘
+     */
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, KC_F12,  KC_F7,   KC_F8,   KC_F9,   XXXXXXX,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, KC_F11,  KC_F4,   KC_F5,   KC_F6,   XXXXXXX,                               XXXXXXX, KC_RSFT, KC_RCMD, KC_ROPT, KC_RCTL, XXXXXXX,
+    XXXXXXX, KC_F10,  KC_F1,   KC_F2,   KC_F3,   XXXXXXX,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                      XXXXXXX, XXXXXXX,     KC_ESC,  KC_SPC,  KC_TAB,      XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX,
                                                      XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX
   )
 };
